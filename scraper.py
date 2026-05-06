b518fd7a8eaa776b7af4f3d0aeb2625952a266d2
Lauren Shu <113384782+nerualuhs@users.noreply.github.com>
2026-05-06 12:27:13 -0700

    Adding more comments

--- scraper.py
+++ scraper.py
@@ -164,6 +164,15 @@ def scraper(url, resp):
 
 
 def extract_next_links(url, resp):
+    # url: the URL that was used to get the page
+    # resp.url: the actual url of the page
+    # resp.status: the status code returned by the server. 200 is OK, you got the page. Other numbers mean that there was some kind of problem.
+    # resp.error: when status is not 200, you can check the error here, if needed.
+    # resp.raw_response: this is where the page actually is. More specifically, the raw_response has two parts:
+    #         resp.raw_response.url: the url, again
+    #         resp.raw_response.content: the content of the page!
+    # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
+    
     # Pass Bad Response Pages
     if resp.status != 200 or resp.raw_response is None:
         return []
